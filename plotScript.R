bf30e5cadffc30fdd93f66a4a8fac3a83bb34935
KkM Coursera <62966579+kkmcoursera@users.noreply.github.com>
2020-04-10 22:46:35 -0400

    Working copy update.

--- plotScript.R
+++ plotScript.R
@@ -9,8 +9,21 @@ hhPower$Time <- strptime(paste(hhPower$Date, hhPower$Time, sep=" "),
                          "%d/%m/%Y %H:%M:%S")
 hhPower$Date <- as.Date(hhPower$Date, "%d/%m/%Y")
 
-Global_ap <- as.numeric(hhPower$Global_active_power) / 1000
-hist(Global_ap, 
+Gap_KW <- as.numeric(hhPower$Global_active_power) / 1000
+hist(Gap_KW, 
      main = "Global Active Power", xlab = "Global Active Power (kilowatts)", 
      col = "red")
 
+par(mar = c(3.1, 4.1, 1.1, 2.1))
+plot(hhPower$Time, Gap_KW, type = "o", pch = 46,
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+
+plot(hhPower$Time, hhPower$Sub_metering_1, type = "o", pch = 46,
+     ylab = "Energy Sub Metering", xlab = "")
+points(hhPower$Time, hhPower$Sub_metering_2, type = "o", pch = 46, col = "red")
+points(hhPower$Time, hhPower$Sub_metering_3, type = "o", pch = 46, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), pch = 46, lwd = 2)
+
+
+     
